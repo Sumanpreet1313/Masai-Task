c55feb056a1d30d93200085a3763fe68bc247a74
Sumanpreet1313 <aujlasuman40@gmail.com>
2025-01-30 19:36:08 +0530

    Added temperature update script

---
+++ temperature.js
@@ -0,0 +1,8 @@
+// Create an array to store temperatures recorded over 7 days
+let temperatures = [30, 32, 31, 29, 33, 34, 35];
+
+// Access the temperature of the 4th day (index 3) and update it
+temperatures[3] += 2;
+
+// Log the updated temperatures to the console
+console.log(temperatures);
