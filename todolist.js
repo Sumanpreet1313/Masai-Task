2402f24bd6b7acc5678e7b4e725621929d2cbda4
Sumanpreet1313 <aujlasuman40@gmail.com>
2025-01-30 20:06:13 +0530

    Added task management script

---
+++ todolist.js
@@ -0,0 +1,11 @@
+// Create an array to represent a daily to-do list with 5 tasks
+let todoList = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"];
+
+// Add a new task to the beginning of the list
+todoList.unshift("New Task at the Beginning");
+
+// Remove the last task from the list
+todoList.pop();
+
+// Log the updated to-do list to the console
+console.log(todoList);
